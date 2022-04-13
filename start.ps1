9263f47e4e74bab8cbec25fe98549c08e63f9ccf
Mathias Köppchen - Protronic GmbH <m.koeppchen@protronic-gmbh.de>
2022-04-13 12:14:05 +0200

    start script

---
+++ start.ps1
@@ -0,0 +1,19 @@
+#Must be the first statement in your script (not coutning comments)
+param([string]$cmd = "watch") 
+
+if ($cmd -eq "update" || $cmd -eq "upgrade") {
+  if (Test-Path .\wwwroot\CeckedFileInfo.db) { 
+    Remove-Item -r -force .\wwwroot\CeckedFileInfo.db;
+  }  
+  Remove-Item -r -force .\wwwroot\images\out\*;
+}
+
+if ($cmd -eq "upgrade") {
+  dotnet ef migrations remove --force;
+  dotnet ef migrations add InitialCreate;
+}
+if ($cmd -eq "update") {
+  dotnet ef database update;
+}
+
+dotnet watch;
