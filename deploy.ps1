c89fc4743c19032107f1a690e65f7d8f317e4ab2
Robert Seidler <r.seidler@protronic-gmbh.de>
2022-06-08 10:33:42 +0200

    added deploy script;

---
+++ deploy.ps1
@@ -0,0 +1,15 @@
+param([string]$cmd = "")
+$sess = New-PSSession -ComputerName prot-exchange -Credential admin.mb;
+
+# restart app without rebuilding / reuploading by running ".\publish.ps1 restart"
+if ( $cmd -ne "restart" ){
+  dotnet publish -c Release;
+  Copy-Item -Recurse -Path "bin/Release/net6.0/publish" -Destination "E:/imapi" -ToSession $sess -Force;
+}
+
+Invoke-Command -Session $sess -ScriptBlock {
+  Get-Process "dotnet" -ErrorAction Ignore | Stop-Process -ErrorAction Ignore
+  Set-Location "E:/imapi/publish/";
+  $env:ASPNETCORE_URLS="http://0.0.0.0:5000";
+  Start-Process -Filepath dotnet -ArgumentList "E:/imapi/publish/IMApi.dll" -RedirectStandardOutput stdout.txt -RedirectStandardError stderr.txt;
+};
