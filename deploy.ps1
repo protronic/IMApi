2dc0389dac9f59eeb3856656017fe3d63ff86ec0
Robert Seidler <r.seidler@protronic-gmbh.de>
2022-06-08 11:02:23 +0200

    removed dotnet run

--- deploy.ps1
+++ deploy.ps1
@@ -7,9 +7,9 @@ if ( $cmd -ne "restart" ){
   Copy-Item -Recurse -Path "bin/Release/net6.0/publish" -Destination "E:/imapi" -ToSession $sess -Force;
 }
 
-Invoke-Command -Session $sess -ScriptBlock {
-  Get-Process "dotnet" -ErrorAction Ignore | Stop-Process -ErrorAction Ignore
-  Set-Location "E:/imapi/publish/";
-  $env:ASPNETCORE_URLS="http://0.0.0.0:5000";
-  Start-Process -Filepath dotnet -ArgumentList "E:/imapi/publish/IMApi.dll" -RedirectStandardOutput stdout.txt -RedirectStandardError stderr.txt;
-};
+# Invoke-Command -Session $sess -ScriptBlock {
+#   Get-Process "dotnet" -ErrorAction Ignore | Stop-Process -ErrorAction Ignore
+#   Set-Location "E:/imapi/publish/";
+#   $env:ASPNETCORE_URLS="http://0.0.0.0:5000";
+#   Start-Process -Filepath dotnet -ArgumentList "E:/imapi/publish/IMApi.dll" -RedirectStandardOutput stdout.txt -RedirectStandardError stderr.txt;
+# };
